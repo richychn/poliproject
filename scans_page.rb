5a5b67ebf2d61a1cc4dd1e71cb59900f7552d0f8
Richy Chen <richychn@hotmail.com>
2017-07-13 20:01:41 +0100

    stop words, file to string, and text to array moved/created in scans page. ratio is now nouns/word to reflect the paper

--- scans_page.rb
+++ scans_page.rb
@@ -1,15 +1,32 @@
 require_relative 'part_of_speech'
 require 'pry-byebug'
 require 'date'
+require 'open-uri'
+require 'nokogiri'
 
 def scan_file(filename)
+  text = file_to_string(filename)
+  array = text_to_array(text)
+  array = rm_stop_words!(array)
+  nouns = no_of_part(array, "N")
+  return nouns / array.length.to_f
+end
+
+def rm_stop_words!(array)
+  stop_words = []
+  File.open("terrier-stop.txt", "r").each_line { |line| stop_words << line.delete("\r\n") }
+  return array.delete_if { |word| stop_words.include? word }
+end
+
+def file_to_string(filename)
   text = ""
   File.open(filename, "r").each_line { |line| text += line.gsub("\n", " ") }
-  nouns = no_of_part(text, "N")
-  adjectives = no_of_part(text, "A")
-  p nouns
-  p adjectives
-  return adjectives / nouns.to_f
+  return text
+end
+
+def text_to_array(text)
+  array = text.downcase.gsub(/[^a-z0-9\s]/i, " ").split
+  return array
 end
 
 start_time = Time.now
